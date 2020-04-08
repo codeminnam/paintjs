e0b6e1997129f06b15a49b05d3daf8d721998ac9
cineminnam <cine.minnam@gmail.com>
2020-04-08 22:38:03 +0900

    #2.3 Changing Color
    #2.3 Changing Color

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName("jsColor");
+
+canvas.width = 500;
+canvas.height = 500;
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
@@ -27,6 +31,10 @@ function onMouseMove(event){
 
 }
 
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor
+    ctx.strokeStyle = color;
+}
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
@@ -34,3 +42,5 @@ if(canvas){
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
